bcee8f7d951a7a084c38593a3292644f46e5d8ae
Mehmet Ali Durusoy <mehmetalidurusoy73+github@gmail.com>
2024-03-10 08:44:01 +0300

    Add path, os platform callback

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 import sys
 import json
 import pyttsx3
+import platform
 from tkinter import *
 import speech_recognition as sr
-
-settings={
- "assistant_wd": os.path.realpath(".")+"/"
-}
 working_directory=os.getcwd()
+settings={
+ "assistant_crd": os.path.realpath(".")+"/",
+ "assistant_wd": working_directory,
+ "schema_protocol" : "ankaspark-ai"
+}
 settings.update(json.load(open(os.getcwd()+"/ankaspark.default.json",'r')))
 if os.path.exists(working_directory+"/ankaspark.json"):
  settings.update(json.load(open(os.getcwd()+"/ankaspark.json",'r')))
@@ -34,12 +36,19 @@ class AnkaSpark():
 
    def cli(self):
       print(settings)
-  
+
+   def platformWindows(self):
+      import winreg
+      print("Running Windows Platform....")
+
    def run(self):
-      if self.argv:
-        self.cli()
-      else:
-        window.mainloop()
+     if platform.system()=="Windows":
+      self.platformWindows()
+     if self.argv:
+       self.cli()
+     else:
+      #window.mainloop()
+      print("Running UI")
 
 if __name__ == '__main__':
    ai=AnkaSpark(sys.argv[1::])
